88a2814c6584f69a22fd6f7d1744e78ec34b55e5
Your kaminosekai54 <culpin.alexis@laposte.net>
2023-11-21 12:46:58 +0100

    update usefull function to read datasets properly and rebuilds sentence dataset add function to plot metrics and rebuild bio-bert file to perform evaluation at the label level on all dataset

---
+++ bio-bert-based-model.py
@@ -0,0 +1,128 @@
+## inspired by :
+#https://colab.research.google.com/github/eugenesiow/practical-ml/blob/master/notebooks/Named_Entity_Recognition_BC5CDR.ipynb
+
+################################################################################
+#imports
+# 
+import pandas as pd
+from simpletransformers.ner import NERModel
+from transformers import AutoTokenizer
+import logging
+import time, os
+# from usefull import read_conll
+# from usefull import preprocessConll2003
+from usefull import readDatasetWithSentenceId
+from usefull import plot_metrics
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.preprocessing import MultiLabelBinarizer
+
+
+################################################################################
+# functions
+def evalBioBertBasedModel(datasetName, modelName = "Bio-bert-based"):
+    # train_df = pd.read_csv(f'datasets/{datasetName}/train.tsv', sep = '\t', header = None, keep_default_na = False, names = ['words', 'labels'])
+    train_df = readDatasetWithSentenceId(datasetName, "train")
+    # train_df = read_conll(f'datasets/{datasetName}/train.txt')
+    # test_df = read_conll('./datasets/bc5cdr/test.txt')
+    # test_df = pd.read_csv(f'datasets/{datasetName}/test.tsv', sep = '\t', header = None, keep_default_na = False, names = ['words', 'labels'])
+    test_df = readDatasetWithSentenceId(datasetName, "test")
+    
+    # dev_df = read_conll('./datasets/bc5cdr/dev.txt')
+    # dev_df = pd.read_csv(f'datasets/{datasetName}/dev.tsv', sep = '\t', header = None, keep_default_na = False, names = ['words', 'labels'])
+    dev_df = readDatasetWithSentenceId(datasetName, "dev")
+    
+    # train_df, dev_df, test_df = preprocessConll2003()
+    # print(train_df)
+    # print(dev_df)
+    # print(test_df)
+    # return
+    # Training and Testing the Model
+    #Set up the Training Arguments
+
+    train_args = {
+    'reprocess_input_data': True,
+    'overwrite_output_dir': True,
+    'sliding_window': True,
+    'max_seq_length': 64,
+    'num_train_epochs': 10,
+    'train_batch_size': 32,
+    'fp16': True,
+    'output_dir': './outputs/',
+    'best_model_dir': '/outputs/best_model/',
+    'evaluate_during_training': True,
+    }
+
+    # save a list of all unique label in training data set
+    custom_labels = list(train_df['labels'].unique())
+    # print(custom_labels)
+
+    #Train the Model
+
+    # using the pre-trained BioBERT model (by [DMIS Lab, Korea University](https://huggingface.co/dmis-lab)) 
+    # log level for more or less verbosity
+    # logging.basicConfig(level=logging.Warning)
+    transformers_logger = logging.getLogger('transformers')
+    transformers_logger.setLevel(logging.WARNING)
+    # using the bio BERT pre-trained model.
+    model = NERModel('bert', 'dmis-lab/biobert-v1.1', labels=custom_labels, args=train_args)
+
+    # Train the model
+    start_time = time.time()
+    model.train_model(train_df, eval_data=dev_df)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    hours, remainder = divmod(elapsed_time, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    print("training time", "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds)))
+    # Evaluate the model in terms of accuracy score
+    result, model_outputs, preds_list = model.eval_model(test_df)
+    
+    # make prediction to be able to have entity level evaluation
+    y_pred = model.predict(test_df.words.values.tolist())
+    resultList= [(k,v) for d in [x for line in y_pred[0] for x in line] for k,v in d.items()]
+    tdf = pd.DataFrame(resultList, columns=["words", "labels"])
+    # print(tdf)
+    # Calculate precision, recall, and F1-score globally and for each unique label
+    report = classification_report(test_df['labels'], tdf.labels, target_names=custom_labels, output_dict=True)
+
+    loss_global = result['eval_loss']
+    accuracy_global = result['precision']
+    recall_global = result['recall']
+    f1_score_global = result['f1_score']
+
+    # Create a data frame with the desired output
+    data = {
+        "modelName": [modelName ],
+        "dataset": [datasetName],
+        "loss": [round(loss_global, 5)],
+        "accuracy_global": [round(accuracy_global, 2)],
+        # "precision_global": [round(precision_global *100, 2)],
+        "recall_global": [round(recall_global*100, 2)],
+        "f1_score_global": [round(f1_score_global *100, 2)]
+    }
+    for label in custom_labels:
+        data[f"accuracy_{label}"] = [round(report[label]['precision']*100, 2)]
+        # data[f"precision_{label}"] = [round(report[label]['precision']*100, 2)]
+        data[f"recall_{label}"] = [round(report[label]['recall']*100, 2)]
+        data[f"f1_score_{label}"] = [round(report[label]['f1-score']*100, 2)]
+
+    df = pd.DataFrame(data)
+    if not os.path.isdir("./results/"): os.makedirs("./results/")
+    if not os.path.isdir(f'results/{datasetName}/'): os.makedirs(f'results/{datasetName}/')
+    df.to_csv(f'results/{datasetName}/{datasetName}_{modelName }_eval.csv', sep=",", index=False)
+    return df
+
+if __name__ == '__main__':
+    model_name="Bio-bert-based"
+    metricList = ["accuracy", "recall", "f1_score"]
+    for dataset in os.listdir("datasets/"):
+        print(dataset)
+        df = evalBioBertBasedModel(datasetName=dataset)
+        for metric in metricList:
+            plot_metrics(df, model_name=model_name, dataset_name=dataset, metric=metric, show = False)
+            
+            
+    # df = evalBioBertBasedModel("ncbi-disease")
+    # plot_metrics(df, model_name="Bio-bert-based", dataset_name="ncbi-disease", metric="accuracy")
+    # evalBioBertBasedModel("ontonote")
--- usefull.py
+++ usefull.py
@@ -5,7 +5,9 @@ from pathlib import Path
 from transformers import BertTokenizerFast
 import torch
 import numpy as np
-
+import chardet
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def download_file(url, output_file):
   Path(output_file).parent.mkdir(parents=True, exist_ok=True)
@@ -16,7 +18,7 @@ if not os.path.isfile('./datasets/bc5cdr/test.txt') : download_file('https://raw
 if not os.path.isfile('./datasets/bc5cdr/dev.txt') : download_file('https://raw.githubusercontent.com/shreyashub/BioFLAIR/master/data/ner/bc5cdr/dev.txt', './datasets/bc5cdr/dev.txt')
 
 
-def read_conll(filename):
+def read_bc5CDR(filename):
     df = pd.read_csv(filename,
                     sep = '\t', header = None, keep_default_na = False,
                     names = ['words', 'pos', 'chunk', 'labels'],
@@ -27,6 +29,33 @@ def read_conll(filename):
     return df[df.words != '']
 
 
+def convertBc5CDRToCONLL(filename):
+    df = pd.read_csv(filename,
+                    sep = '\t', header = None, keep_default_na = False,
+                    names = ['words', 'pos', 'chunk', 'labels'],
+                    quoting = 3, skip_blank_lines = False)
+    df = df[~df['words'].astype(str).str.startswith('-DOCSTART- ')] # Remove the -DOCSTART- header
+    finalDf = df[["words", "labels"]]
+    finalDf.to_csv(filename.replace(".tsv", ".tmp"), sep="\t", index=False, header=False)
+    with open(filename.replace(".tsv", ".tmp"), 'r') as input_file, open(filename, 'w') as output_file:
+        for i, line in enumerate(input_file):
+            if line.strip("\n").strip(" ") == '\t' and i == 0: continue
+            if line.strip("\n").strip(" ") == '\t': output_file.write('\n')
+            else: output_file.write(line)
+            
+    os.remove(filename.replace(".tsv", ".tmp"))
+
+
+def readDatasetWithSentenceId(datasetName, type):
+    df = pd.read_csv(f'datasets/{datasetName}/{type}.tsv', sep = '\t', header = None, keep_default_na = False, names = ['words', 'labels'], quoting = 3, skip_blank_lines = False)
+    df = df[~df['words'].astype(str).str.startswith('-DOCSTART- ')] # Remove the -DOCSTART- header
+    df['sentence_id'] = (df.words == '').cumsum()
+    df['sentence_id'] = df['sentence_id'] +1
+    # print(df[df.words != ''])
+    return df[df.words != '']
+    
+# readDatasetWithSentenceId("bc5cdr", "train")
+# read_bc5CDR("datasets/bc5cdr/original/train.txt")
 def align_label(texts, labels):
     tokenized_inputs = tokenizer(texts, padding='max_length', max_length=512, truncation=True)
     word_ids = tokenized_inputs.word_ids()
@@ -103,7 +132,10 @@ def preprocessConll2003(datasetPath = "./datasets/conll2003/conll2003.csv"):
 # preprocessConll2003()
 
 def reassembleDataset(datasetPath, sep = "\t"):
-    with open(datasetPath, "r") as file :
+    result = ""
+    with open(datasetPath, "rb") as file :
+        result = chardet.detect(file.read())
+    with open(datasetPath, "r", encoding=result['encoding']) as file :
         sentence = []
         labels = []
         newDataset = {}
@@ -121,7 +153,67 @@ def reassembleDataset(datasetPath, sep = "\t"):
                 
         if len(sentence) > 0 : newDataset[" ".join(sentence)] = " ".join(labels)
         df = pd.DataFrame.from_dict({"text": newDataset.keys(), "labels": newDataset.values()})
-        df.to_csv(datasetPath[: datasetPath.rfind(".")] + "_assembled.csv", sep=sep, index=False)
+        df.to_csv(datasetPath[: datasetPath.rfind(".")] + "_assembled.tsv", sep=sep, index=False)
             
         
         
+        
+def assembleAllDataSet():
+    for dataset in os.listdir("datasets/"):
+        print(dataset)
+        subpath= "datasets/" + dataset + "/"
+        fileList = os.listdir(subpath)
+        if not f'{dataset}_assembled' in fileList:
+            os.makedirs(subpath + dataset+"_assembled/")
+            for file in fileList:
+                if file.endswith(".tsv") :
+                    print(file)
+                    reassembleDataset(subpath + file)
+                    os.renames(subpath + file.replace (".tsv", "_assembled.tsv"), subpath + dataset+"_assembled/" + file.replace (".tsv", "_assembled.tsv"))
+                
+                
+                
+
+def plot_metrics(df, model_name, dataset_name, metric, show = True):
+    results_folder = "results/"
+    dataset_folder = f'{results_folder}{dataset_name}/'
+    figures_folder = f'{dataset_folder}figures/'
+    
+    # Create directories if they don't exist
+    for folder in [results_folder, dataset_folder, figures_folder]:
+        if not os.path.isdir(folder): os.makedirs(folder)
+    
+    # Extract column names starting  with '_accuracy'
+    metric_columns = [col for col in df.columns if col.startswith(f'{metric}_')]
+
+    # Set up Seaborn with a diverging color palette
+    sns.set(style="whitegrid")
+    colors = sns.color_palette("RdYlGn", len(metric_columns))
+
+    # Plotting the bar chart with seaborn
+    plt.figure(figsize=(10, 6))
+    ax = sns.barplot(x=metric_columns, y=df.iloc[0][metric_columns], palette=colors)
+
+    # Customize plot appearance
+    plt.title(f'{model_name} {metric} Comparison for {dataset_name}', fontsize=16)
+    plt.xlabel('Entity type', fontsize=14)
+    plt.ylabel(metric, fontsize=14)
+    plt.ylim(0, 110)  # Set y-axis limit to percentages (0-100)
+
+    # Rotate x-axis labels for better readability
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
+
+    # Add legend
+    plt.legend([col.replace(f'{metric}_', "") for col in metric_columns], title="Labels List", loc="upper left")
+
+    # Add grid lines
+    plt.grid(True, linestyle='--', alpha=0.7)
+
+    # Increase tick label font size
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    # Save the plot with tight layout to prevent label overlaps
+    plt.tight_layout()
+    if show == True : plt.show()
+    plt.savefig(f'{figures_folder}{model_name}_{metric}_plot_for_{dataset_name}.png')

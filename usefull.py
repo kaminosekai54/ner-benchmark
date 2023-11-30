25cfd51191bf83f56e3d4e4c1cf2ab89ab395af7
Your kaminosekai54 <culpin.alexis@laposte.net>
2023-11-30 16:30:42 +0100

    adding figures and modifying usefull to add more function

--- usefull.py
+++ usefull.py
@@ -206,7 +206,7 @@ def correctLabels(datasetName, modelName):
     report = classification_report(df1.labels, df2.labels, labels=custom_labels, target_names=custom_labels, output_dict=True)
     data = {
         "modelName": [modelName ],
-        "datasetName": [datasetName],
+        "dataset": [datasetName],
         "accuracy_global": [round(report['weighted avg']['precision']*100, 2)],
         "recall_global": [round(report['weighted avg']['recall']*100, 2)],
         "f1_score_global": [round(report['weighted avg']['f1-score'] *100, 2)]
@@ -226,19 +226,111 @@ def getEncoding(filePath):
     
 def getGlobalModelResults(modelName):
     dfList = []
-    for dataset in os.listdir("results/"):
-        #print(dataset)
+    for dataset in [folder for folder in os.listdir("results/") if os.path.isdir(f'results/{folder}')]:
         for file in os.listdir(f'results/{dataset}/'):
             if modelName in file and file.endswith("eval.csv"):
-                #print(file)
                 tmpDf = pd.read_csv(f'results/{dataset}/{file}')
                 tmpDf = tmpDf[["modelName", "dataset", "accuracy_global", "recall_global", "f1_score_global"]]
                 dfList.append(tmpDf)
 
     finalDf = pd.concat(dfList, ignore_index=True)
-    #print(len(dfList))
-    #print(len(finalDf))
-    #print(finalDf)
     return finalDf
 
 
+def getGlobalCorrectedModelResults(modelName):
+    dfList = []
+    for dataset in [folder for folder in os.listdir("results/") if os.path.isdir(f'results/{folder}')]:
+        for file in os.listdir(f'results/{dataset}/'):
+            if modelName in file and file.endswith("eval_corrected.csv"):
+                tmpDf = pd.read_csv(f'results/{dataset}/{file}')
+                tmpDf = tmpDf[["modelName", "dataset", "accuracy_global", "recall_global", "f1_score_global"]]
+                dfList.append(tmpDf)
+
+    finalDf = pd.concat(dfList, ignore_index=True)
+    return finalDf
+
+
+import numpy as np
+
+def plotGlobalMetricsInOnePlot(df, modelList, metricList, suffixe ="", show=True):
+    # Set up an improved color mapping for each dataset
+    color_mapping = {
+        'bc5cdr': '#1f78b4',  # blue
+        'jnlpba': '#6a3d9a',  # purple
+        'ncbi-disease': '#ff7f00',  # orange
+        'ontonote': '#e31a1c',  # red
+        'species800': '#fdbf6f'  # yellow
+    }
+
+    # Set up figure and axis
+    fig, ax = plt.subplots(nrows=len(modelList), ncols=len(metricList), figsize=(15, 10))
+
+    # Plot the specified metric for each dataset across models
+    for i, model in enumerate(modelList):
+        modelData = df[df.modelName == model]
+        for j, metric in enumerate(metricList):
+            metricData = modelData[["dataset", metric]]
+            x = np.arange(len(metricData))
+
+            for k, dt in enumerate(metricData.dataset):
+                color = color_mapping.get(dt, 'gray')
+                datasetData = metricData[metricData.dataset == dt]
+                ax[i, j].bar(x[k], datasetData[metric], label=dt, color=color, alpha=0.7)
+
+    # Set labels, title, and legend
+    for i, ax_row in enumerate(ax):
+        for j, ax_col in enumerate(ax_row):
+            ax_col.set_xlabel('Datasets')
+            ax_col.set_ylabel(str.upper(metricList[j]).replace("_", " "))
+            ax_col.set_xticks(np.arange(len(df.dataset.unique())))
+            ax_col.set_xticklabels(df.dataset.unique().tolist(), rotation=45)
+            ax_col.set_title(f'{modelList[i]} - {str.upper(metricList[j]).replace("_", " ")}')
+
+    handles, labels = ax[0, 0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=5, title='Datasets')
+
+    # Show the plot
+    plt.suptitle(f'Comparison of global {suffixe} evaluation metrics on all datasets for each model', y=1.05, fontsize=16)
+    plt.tight_layout()
+    plt.savefig(f'results/global_{suffixe}_Metric_Evaluation.jpg')
+    if show == True : plt.show()
+
+
+
+def describeDataSet(datasetName):
+    description = f'{str.upper(datasetName)} : \n'
+    trainDf = readDatasetWithSentenceId(datasetName, "train")
+    devDf = rseadDatasetWithSentenceId(datasetName, "dev")
+    testDf = readDatasetWithSentenceId(datasetName, "test")
+    fullDf = pd.concat([trainDf, devDf, testDf], ignore_index=True)
+    train_assembled = pd.read_csv(f'datasets/{datasetName}/{datasetName}_assembled/train_assembled.tsv', sep="\t")
+    dev_assembled = pd.read_csv(f'datasets/{datasetName}/{datasetName}_assembled/dev_assembled.tsv', sep="\t")
+    test_assembled = pd.read_csv(f'datasets/{datasetName}/{datasetName}_assembled/test_assembled.tsv', sep="\t")
+    full_assembled = pd.concat([train_assembled, dev_assembled, test_assembled], ignore_index=True)
+    description += f'This dataset is composed of 3 file, \n'
+    description += f'Training : \n{len(trainDf)} words for a total of {len(train_assembled)} sentences. \n'
+    description += f'Dev : \n{len(devDf)} words for a total of {len(dev_assembled)} sentences. \n'  
+    description +=  f'Testing : \n{len(testDf)} words for a total of {len(test_assembled)} sentences. \n'  
+    description +=  f'Which represent in total {len(fullDf)} words for a total of {len(full_assembled)} sentences. \n'
+    description +=  f'It covers the following entity labels : \n'
+    for label in fullDf.labels.unique():
+        description += f'{label} : {len(fullDf[fullDf.labels == label])} entities.\n'
+    description += "the highest sota f1-score for this data set is at "
+    return description
+
+# models_to_plot = ['bert-based', 'Bio-bert-based', 'spark-nlp']
+# metric_to_plot = ['f1_score_global', "recall_global", "accuracy_global"]
+# df1 = getGlobalModelResults("bert-based")
+# df2 = getGlobalModelResults("Bio-bert-based")
+# df3 = getGlobalModelResults("spark-nlp")
+# df = pd.concat([df1, df2, df3], ignore_index=True).drop_duplicates()
+# df.to_csv(f'results/global_perf.csv', index=False)
+# plotGlobalMetricsInOnePlot(df, models_to_plot, metric_to_plot, show=False)
+
+# df1 = getGlobalCorrectedModelResults("bert-based")
+# df2 = getGlobalCorrectedModelResults("Bio-bert-based")
+# df3 = getGlobalCorrectedModelResults("spark-nlp")
+# df = pd.concat([df1, df2, df3], ignore_index=True).drop_duplicates()
+# df.to_csv(f'results/global_corrected_perf.csv', index=False)
+# Example usage:
+# plotGlobalMetricsInOnePlot(df, models_to_plot, metric_to_plot, suffixe="corrected", show=False)
